f17ddc4a214bcb7b7f89ebb5e0e0df930a09713b
Mizael Sousa <aluno@MacBook-Pro-de-Aluno.local>
2019-02-27 21:13:55 -0400

    App mudanca de fonte randomica

---
+++ LabelFonts/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LabelFonts
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+        
+        string[] frases = {  
+            "ShadowsIntoLight.ttf#ShadowsIntoLight",
+            "IndieFlower.ttf#IndieFlower",
+            "Pacifico-Regular.ttf#Pacifico-Regular"
+        };
+        
+        void MudarFonte(Object sender, EventArgs e){
+
+            Label_01.Text = frases[random.Next(0, (frases.Length - 1))];
+            Label_02.Text = frases[random.Next(0, (frases.Length - 1))];
+            Label_03.Text = frases[random.Next(0, (frases.Length - 1))];
+
+        }
+    }
+}
